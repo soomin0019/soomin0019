aefffad94ff8f1b12ab066d121659ceb03a647d4
soomin0019 <113010528+soomin0019@users.noreply.github.com>
2023-07-11 22:17:45 +0900

    Create 11382.java

---
+++ Java_BJ/11382.java
@@ -0,0 +1,13 @@
+package algorithm;
+import java.util.*;
+
+public class Beakjoon {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        long x = a.nextLong();
+        long y = a.nextLong();
+        long z = a.nextLong();
+
+        System.out.println(x+y+z);
+    }
+}
